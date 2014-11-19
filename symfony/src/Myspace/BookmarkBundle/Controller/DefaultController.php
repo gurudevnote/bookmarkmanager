005cc706a28231aa6de3c9bdbbfae214d7708f56
Nguyen Huu Huy <huuhuy@gmail.com>
2014-11-19 23:30:56 +0700

    Add contact form

--- symfony/src/Myspace/BookmarkBundle/Controller/DefaultController.php
+++ symfony/src/Myspace/BookmarkBundle/Controller/DefaultController.php
@@ -2,12 +2,71 @@
 
 namespace Myspace\BookmarkBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Myspace\BookmarkBundle\Entity\Bookmark;
+use Myspace\BookmarkBundle\Entity\Category;
+use Myspace\BookmarkBundle\Entity\Tag;
+use Myspace\BookmarkBundle\Entity\Comment;
+use Myspace\BookmarkBundle\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('MyspaceBookmarkBundle:Default:index.html.twig', array('name' => $name));
+        //test entity
+		
+		$catparent = $this->getDoctrine()
+				->getRepository('MyspaceBookmarkBundle:Category')
+				->find(1);
+		
+		//var_dump($catparent->getChildren());exit;
+		foreach($catparent->getChildren() as $it)
+		{
+			echo $it->getName()."<br/>";
+		}
+		exit;
+		
+		$category = new Category();
+		$category->setName("course");
+		$category->setParent($catparent);
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($category);
+		//$em->persist($product);
+		$em->flush();		
+		
+		$catparent1 = $this->getDoctrine()
+				->getRepository('MyspaceBookmarkBundle:Category')
+				->find(2);
+		//var_dump($catparent1);exit;
+		
+		//return $this->render('MyspaceBookmarkBundle:Default:index.html.twig', array('name' => $name));
     }
+	
+	public function importAction(Request $request)
+	{
+		$contact = new Contact();
+		$contact->setEmail("huuhuy@gmail.com");
+		$contact->setName("Contact form");
+		$form = $this->createFormBuilder($contact)
+				//->add('subject','subject')
+				//->add('email', 'email')
+				//->add('name','name')
+				//->add('description','description')
+				//->add('title', 'title')
+				->add('save', 'submit', array('label' => 'Create Post'))
+				->getForm();
+				
+		// create a task and give it some dummy data for this example
+		$task = new Contact();
+		$task->setTask('Write a blog post');
+		$task->setDueDate(new \DateTime('tomorrow'));
+		$form = $this->createFormBuilder($task)
+		->add('task', 'text')
+		->add('dueDate', 'date')
+		->add('save', 'submit', array('label' => 'Create Post'))
+		->getForm();			
+		return $this->render("MyspaceBookmarkBundle:Default:import.html.twig", array("form"=>$form->createView(),));
+	}
 }
---
+++ symfony/src/Myspace/BookmarkBundle/Entity/Contact.php
@@ -0,0 +1,70 @@
+<?php
+// src\Myspace\BookmarkBundle\Entity\Document.php
+namespace Myspace\BookmarkBundle\Entity;;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact
+{
+	protected $name;
+	protected $email;
+	protected $title;
+	protected $description;
+	protected $subject;
+	protected $task;
+	protected $dueDate;
+	
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function getTitle(){
+		return $this->title;
+	}
+
+	public function setTitle($title){
+		$this->title = $title;
+	}
+
+	public function getSubject(){
+		return $this->subject;
+	}
+
+	public function setSubject($subject)
+	{
+		$this->subject = $subject;
+	}	
+
+	public function getTask()
+	{
+		return $this->task;
+	}
+	
+	public function setTask($task)
+	{
+		$this->task = $task;
+	}
+	
+	public function getDueDate()
+	{
+		return $this->dueDate;
+	}
+	
+	public function setDueDate(\DateTime $dueDate = null)
+	{
+		$this->dueDate = $dueDate;
+	}
+}
