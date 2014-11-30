47cbaa9a76f0c0793a61238c0f7e8a1e8ea10a60
Nguyen Huu Huy <huuhuy@gmail.com>
2014-12-01 01:25:29 +0700

    reorganize angularjs app directory structure

---
+++ symfony/web/js/app.js
@@ -0,0 +1,29 @@
+var bookmarkApp = angular.module('bookmarkApp', [
+	'ngRoute',
+	'bookmarkController'
+]);
+
+bookmarkApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/categories', {
+				templateUrl: 'views/category-list.html',
+				controller: 'CategoryListCtrl'
+			}).
+			when('/categories/:categoryId', {
+				templateUrl: 'views/category-detail.html',
+				controller: 'CategoryDetailCtrl'
+			}).
+			when('/bookmarks', {
+				templateUrl: 'views/bookmark-list.html',
+				controller: 'BookmarkListCtrl'
+			}).
+			when('/bookmarks/:bookmarkId', {
+				templateUrl: 'views/bookmark-detail.html',
+				controller: 'BookmarkDetailCtrl'
+			}).
+			otherwise({
+				redirectTo: '/categories'
+			});
+	}
+]);
